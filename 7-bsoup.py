66e6b9cc0dfe1faa72e2b1e244e19c566344a0ff
Arthur-San <arthursantos242568@gmail.com>
2024-02-07 08:47:33 -0300

    bsoup

--- 7-bsoup.py
+++ 7-bsoup.py
@@ -21,9 +21,7 @@ with open('pages/index.html', 'r', encoding='utf-8') as file_html:
 course_cards = soup.find_all('div',class_='card')
 # print(course_cards)
 for course in course_cards:
-    # print(course.h5.text)
-    # print(course.p.text)
-    # print(course.a.text.split()[-1])
+    
     
     name = course.h5.text
     description = course.p.text
